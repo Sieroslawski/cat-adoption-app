a648cadecb743404c7c10eb343e2f75241b061a8
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-20 15:18:35 -0700

    banner added and positioned

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import banner from '../images/banner.jpg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="about-wrapper">
+      <img src={banner} id="cat-img" alt="cat-img"></img>
+    </div>
   )
 }
 
