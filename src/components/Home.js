47748a2b6868683721cb62fc332966954b8f3bd2
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-20 15:35:56 -0700

    added FA icons above banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import banner from '../images/banner.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import styled from "styled-components";
 
 function Home() {
   return (
     <div className="about-wrapper">
-      <img src={banner} id="cat-img" alt="cat-img"></img>
+      <div className="paws">
+        <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>
+        <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>     
+        <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>     
+        <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>
+        <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>
+        <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>
+        </div>
+      <img src={banner} id="cat-img" alt="cat-img"></img>      
     </div>
   )
 }
