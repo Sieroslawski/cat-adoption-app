f5e9ecb63fc23db2351e0ec0907c64f780285461
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-20 17:08:10 -0700

    button styling complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import banner from '../images/banner.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
-import styled from "styled-components";
+import { NavLink } from 'react-router-dom'
 
 function Home() {
   return (
@@ -15,7 +15,9 @@ function Home() {
         <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>
         <FontAwesomeIcon icon={solid('paw')} size="4x" className="cat-icon"/>
         </div>
-      <img src={banner} id="cat-img" alt="cat-img"></img>      
+      <img src={banner} id="cat-img" alt="cat-img"></img>
+      <div class="centered-btn"><NavLink to="/adoption" className={(navData => (navData.isActive ? "header-li" : 'none'))} id="btn1"><li>View Cats</li></NavLink></div>
+      <div class="centered-btn1"> <NavLink to="/login" className={(navData => (navData.isActive ? "header-li" : 'none'))} id="btn2"><li>Login</li></NavLink></div>
     </div>
   )
 }
