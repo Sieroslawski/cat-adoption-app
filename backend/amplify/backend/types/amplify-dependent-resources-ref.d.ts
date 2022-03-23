07f7a072213dd13c15af4ca7c78d38ec13ac9f39
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-23 15:08:43 -0700

    basic api created with amplify

---
+++ backend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "catPostCats": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "catPost": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
